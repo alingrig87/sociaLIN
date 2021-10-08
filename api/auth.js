ad37081b01e8238835866ed428324157d015d9fb
Alin Grigorescu <alin.grig87@gmail.com>
2021-10-09 01:47:07 +0300

    [Auth route]
    - implement the auth route to response with authenticated user based on JWT

--- api/auth.js
+++ api/auth.js
@@ -2,9 +2,19 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 
+const User = require('../models/User');
+
 // @route         GET api/auth
 // @description   Test route
 // @access        Public
-router.get('/', auth, (request, response) => response.send('Test Auth Route'));
+router.get('/', auth, async (request, response) => {
+	try {
+		const user = await User.findById(request.user.id).select('-password');
+		response.json(user);
+	} catch (error) {
+		console.log(error.message);
+		response.status(500).send('Server error');
+	}
+});
 
 module.exports = router;
