8084453de046182af16942457111e01c08836a4c
Alin Grigorescu <alin.grig87@gmail.com>
2021-10-09 21:13:15 +0300

    [Authentication route | Email and Password validation]
    - implement authentication
    - check if the password is valid using bcrypt
    - check if the email is valid
    - return the token for valid users and "Invalid credential" error for invalid email or password

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
@@ -16,5 +20,70 @@ router.get('/', auth, async (request, response) => {
 		response.status(500).send('Server error');
 	}
 });
+
+// @route         GET api/auth
+// @description   User authentication | get jwt token
+// @access        Public
+
+router.post(
+	'/',
+	[
+		check('email', 'Please use a valid email').isEmail(),
+		check('password', 'Enter the valid password').exists(),
+	],
+	async (request, response) => {
+		console.log(request.body);
+		const errors = validationResult(request);
+		if (!errors.isEmpty()) {
+			return response.status(400).json({ errors: errors.array() });
+		}
+
+		const { name, email, password } = request.body;
+
+		try {
+			// check if user exists
+			let user = await User.findOne({ email: email });
+			if (!user) {
+				return response
+					.status(400)
+					.json({ errors: [{ msg: 'Invalid credentials' }] });
+			}
+
+			const isValidPassword = await bcrypt.compare(password, user.password);
+
+			if (!isValidPassword) {
+				return response
+					.status(400)
+					.json({ errors: [{ msg: 'Invalid credentials' }] });
+			}
+
+			// return jsonwebtoken
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: 360000 },
+				(err, token) => {
+					if (err) throw err;
+					response.json({ token });
+				}
+			);
+
+			// response.send('User registered');
+		} catch (error) {
+			console.error(error.message);
+			response.status(500).send('Server error');
+		}
+
+		// response.send('Test Users Route');
+	}
+);
+
+module.exports = router;
 
 module.exports = router;
