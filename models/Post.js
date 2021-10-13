9e74e4599ec42c5b0d234a9bebcda8614ea01604
Alin Grigorescu <alin.grig87@gmail.com>
2021-10-13 11:25:18 +0300

    [Post model]
    - implement post model

---
+++ models/Post.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const PostsSchema = mongoose.Schema({
+	user: {
+		// the user will be able to delete his own posts
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'user',
+	},
+	text: {
+		type: String,
+		required: true,
+	},
+	// name of the user
+	name: {
+		type: String,
+	},
+	avatar: {
+		type: String,
+	},
+	likes: [
+		{
+			user: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'users',
+			},
+		},
+	],
+	comments: [
+		{
+			user: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'users',
+			},
+			text: {
+				type: String,
+				required: true,
+			},
+			name: {
+				type: String,
+			},
+			avatar: {
+				type: String,
+			},
+			date: {
+				type: Date,
+				default: Date.now,
+			},
+		},
+	],
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
